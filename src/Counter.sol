231de60be8679894aeec929191f813636fe3675c
thryec <thryec@gmail.com>
2022-05-22 22:07:52 +0800

    basic counter tests in solidity with foundry

---
+++ src/Counter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+    address public immutable owner;
+    uint256 public count;
+
+    event Update(address caller, uint256 newNumber);
+
+    constructor(uint256 _initNumber) {
+        count = _initNumber;
+        owner = msg.sender;
+    }
+
+    function increment() public {
+        require(msg.sender == owner);
+        count++;
+        emit Update(msg.sender, count);
+    }
+
+    function decrement() public {
+        require(msg.sender == owner);
+        count--;
+        emit Update(msg.sender, count);
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,34 @@
 pragma solidity ^0.8.13;
 
 import "ds-test/test.sol";
+import {Counter} from "src/Counter.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Counter counter;
+    address sender;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        // console.log("testing counter");
+        counter = new Counter(3);
+        sender = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+    }
+
+    function testInitCount() public {
+        uint256 currentNum = counter.count();
+        assertTrue(currentNum == 3);
+    }
+
+    function testIncrement() public {
+        counter.increment();
+        assert(counter.count() == 4);
+    }
+
+    function testDecrement() public {
+        counter.decrement();
+        assert(counter.count() == 2);
+    }
+
+    function testInitOwner() public {
+        assertTrue(counter.owner() == sender);
     }
 }
