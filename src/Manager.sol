597f1885c9b0fe3200b192a47a8ae1eab0b3e94d
thryec <thryec@gmail.com>
2022-06-04 15:43:47 +0800

    directly import erc20 into token manager

--- src/Manager.sol
+++ src/Manager.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
-import "./Token.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Manager is Token, AccessControl {
+contract Manager is ERC20, Ownable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor(address minter) {
+    constructor(address minter) ERC20("Social Credit", "SOC") {
         _setupRole(MINTER_ROLE, minter);
     }
 }
--- src/Token.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.13;
-
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token is ERC20, Ownable {
-    constructor() ERC20("Social Credit", "SOC") {}
-}
